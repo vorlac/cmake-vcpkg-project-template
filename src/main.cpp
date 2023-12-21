472ec7fd1edc50db3177ae3dd4e4acdaac09a76c
sal <sal@orlac.io>
2023-12-21 16:15:24 -0500

    sdl2 example - main.cpp copied from: https://github.com/xyproto/sdl2-examples

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,77 @@
-#include <cstdint>
+#include <cstdlib>
 #include <iostream>
-#include <string_view>
 
-#include <Zydis/MetaInfo.h>
-#include <fmt/color.h>
-#include <fmt/format.h>
-#include <fmt/std.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_main.h>
 
 int main(int argc, char** argv)
 {
-    constexpr std::string_view hi{ "hello world" };
-    fmt::print(fmt::emphasis::blink | fmt::fg(fmt::color::coral), "{}\n", hi);
+    using std::cerr;
+    using std::endl;
 
-    auto test = ZydisCategoryGetString(ZydisInstructionCategory::ZYDIS_CATEGORY_AES);
-    fmt::print(fmt::emphasis::underline | fmt::fg(fmt::color::beige), "Test str: {}\n", test);
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        cerr << "SDL_Init Error: " << SDL_GetError() << endl;
+        return EXIT_FAILURE;
+    }
 
-    return 0;
+    SDL_Window* win = SDL_CreateWindow("Hello World!", 100, 100, 620, 387, SDL_WINDOW_SHOWN);
+    if (win == nullptr)
+    {
+        cerr << "SDL_CreateWindow Error: " << SDL_GetError() << endl;
+        return EXIT_FAILURE;
+    }
+
+    SDL_Renderer* ren = SDL_CreateRenderer(win, -1,
+                                           SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (ren == nullptr)
+    {
+        cerr << "SDL_CreateRenderer Error" << SDL_GetError() << endl;
+        if (win != nullptr)
+            SDL_DestroyWindow(win);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
+    SDL_Surface* bmp = SDL_LoadBMP("resources/grumpy-cat.bmp");
+    if (bmp == nullptr)
+    {
+        cerr << "SDL_LoadBMP Error: " << SDL_GetError() << endl;
+        if (ren != nullptr)
+            SDL_DestroyRenderer(ren);
+        if (win != nullptr)
+            SDL_DestroyWindow(win);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, bmp);
+    if (tex == nullptr)
+    {
+        cerr << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << endl;
+        if (bmp != nullptr)
+            SDL_FreeSurface(bmp);
+        if (ren != nullptr)
+            SDL_DestroyRenderer(ren);
+        if (win != nullptr)
+            SDL_DestroyWindow(win);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+    SDL_FreeSurface(bmp);
+
+    for (int i = 0; i < 20; i++)
+    {
+        SDL_RenderClear(ren);
+        SDL_RenderCopy(ren, tex, nullptr, nullptr);
+        SDL_RenderPresent(ren);
+        SDL_Delay(100);
+    }
+
+    SDL_DestroyTexture(tex);
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
 }
