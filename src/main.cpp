6de1c7be2f927f1897679af32f7d0496a38025b7
sal <sal@orlac.io>
2024-01-28 10:47:50 -0500

    simple boost example

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,14 @@
-#include <cstdint>
-#include <iostream>
-#include <string_view>
 
-#include <fmt/color.h>
-#include <fmt/format.h>
-#include <fmt/std.h>
-#include <openssl/crypto.h>
+#include <print>
+
+#include <boost/algorithm/is_palindrome.hpp>
 
 int main(int argc, char** argv)
 {
-    constexpr std::string_view hi{ "hello world" };
-    fmt::print(fmt::emphasis::blink | fmt::fg(fmt::color::coral), "{}\n", hi);
-
-    const uint32_t ssl_major{ OPENSSL_version_major() };
-    const uint32_t ssl_minor{ OPENSSL_version_minor() };
-    const uint32_t ssl_patch{ OPENSSL_version_patch() };
-
-    fmt::print(fmt::emphasis::underline | fmt::fg(fmt::color::beige),
-               "Using OpenSSL Version: {}.{}.{}\n", ssl_major, ssl_minor, ssl_patch);
-
-    std::cin.get();
-
+    for (auto&& str : { "asdf", "racecar" })
+    {
+        bool is_pal{ boost::algorithm::is_palindrome(str) };
+        std::print("{} is palindrome? - {}\n", str, is_pal);
+    }
     return 0;
 }
