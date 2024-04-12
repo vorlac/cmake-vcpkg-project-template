c213586e45cbe95d2b0b22bd44942e715a3e6feb
sal <sal@orlac.io>
2024-04-12 07:20:05 -0400

    fdsfdsd

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ int main(int argc, char** argv)
 {
     constexpr std::string_view hi{ "hello world" };
     fmt::print(fmt::emphasis::blink | fmt::fg(fmt::color::coral), "{}\n", hi);
+
     auto test = ZydisCategoryGetString(ZydisInstructionCategory::ZYDIS_CATEGORY_AES);
     fmt::print(fmt::emphasis::underline | fmt::fg(fmt::color::beige), "Test str: {}\n", test);
 
