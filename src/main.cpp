a7c0fac8ebb4b579b692f47a5fdd07ba8e9270e0
sal <sal@orlac.io>
2024-07-12 00:50:28 -0400

    added sdl3 example

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,86 @@
-#include <cstdint>
+#include <cstdlib>
 #include <iostream>
-#include <string_view>
 
-#include <fmt/color.h>
-#include <fmt/format.h>
-#include <fmt/std.h>
-#include <openssl/crypto.h>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_mixer/SDL_mixer.h>
+#include <SDL3_ttf/SDL_ttf.h>
 
 int main(int argc, char** argv)
 {
-    constexpr std::string_view hi{ "hello world" };
-    fmt::print(fmt::emphasis::blink | fmt::fg(fmt::color::coral), "{}\n", hi);
+    using std::cerr;
+    using std::endl;
 
-    const uint32_t ssl_major{ OPENSSL_version_major() };
-    const uint32_t ssl_minor{ OPENSSL_version_minor() };
-    const uint32_t ssl_patch{ OPENSSL_version_patch() };
+    if (SDL_Init(0x0001FFFF) != 0)
+    {
+        cerr << "SDL_Init Error: " << SDL_GetError() << endl;
+        return EXIT_FAILURE;
+    }
 
-    fmt::print(fmt::emphasis::underline | fmt::fg(fmt::color::beige),
-               "Using OpenSSL Version: {}.{}.{}\n", ssl_major, ssl_minor, ssl_patch);
+    auto flags = IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF | IMG_INIT_WEBP;
+    if (IMG_Init(flags) != flags)
+    {
+        cerr << "IMG_Init Error: " << SDL_GetError() << endl;
+        return EXIT_FAILURE;
+    }
 
-    std::cin.get();
+    SDL_Window* win = SDL_CreateWindow("Hello World!", 620, 387, SDL_WINDOW_RESIZABLE);
+    if (win == nullptr)
+    {
+        cerr << "SDL_CreateWindow Error: " << SDL_GetError() << endl;
+        return EXIT_FAILURE;
+    }
 
-    return 0;
+    SDL_Renderer* ren = SDL_CreateRenderer(win, "opengl");
+    if (ren == nullptr)
+    {
+        cerr << "SDL_CreateRenderer Error" << SDL_GetError() << endl;
+        if (win != nullptr)
+            SDL_DestroyWindow(win);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
+    SDL_Surface* bmp = SDL_LoadBMP("resources/grumpy-cat.bmp");
+    if (bmp == nullptr)
+    {
+        cerr << "SDL_LoadBMP Error: " << SDL_GetError() << endl;
+        if (ren != nullptr)
+            SDL_DestroyRenderer(ren);
+        if (win != nullptr)
+            SDL_DestroyWindow(win);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, bmp);
+    if (tex == nullptr)
+    {
+        cerr << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << endl;
+        if (bmp != nullptr)
+            SDL_DestroySurface(bmp);
+        if (ren != nullptr)
+            SDL_DestroyRenderer(ren);
+        if (win != nullptr)
+            SDL_DestroyWindow(win);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+    SDL_DestroySurface(bmp);
+
+    for (int i = 0; i < 20; i++)
+    {
+        SDL_RenderClear(ren);
+        SDL_RenderTexture(ren, tex, nullptr, nullptr);
+        SDL_RenderPresent(ren);
+        SDL_Delay(100);
+    }
+
+    SDL_DestroyTexture(tex);
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
 }
