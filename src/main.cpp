a8b9ea55ebf2867d16bdaebf67ce9d6bad52d285
sal <sal@orlac.io>
2025-01-11 09:41:11 -0500

    mysql-conncetor example

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,10 @@
-#include <cstdint>
-#include <iostream>
-#include <string_view>
+#include <utility>
 
-#include <fmt/color.h>
 #include <fmt/format.h>
-#include <fmt/std.h>
-#include <openssl/crypto.h>
+#include <mysqlx/xapi.h>
 
-int main(int argc, char** argv)
+int main()
 {
-    constexpr std::string_view hi{ "hello world" };
-    fmt::print(fmt::emphasis::blink | fmt::fg(fmt::color::coral), "{}\n", hi);
-
-    const uint32_t ssl_major{ OPENSSL_version_major() };
-    const uint32_t ssl_minor{ OPENSSL_version_minor() };
-    const uint32_t ssl_patch{ OPENSSL_version_patch() };
-
-    fmt::print(fmt::emphasis::underline | fmt::fg(fmt::color::beige),
-               "Using OpenSSL Version: {}.{}.{}\n", ssl_major, ssl_minor, ssl_patch);
-
-    std::cin.get();
-
-    return 0;
+    auto test = mysqlx_opt_type_enum::MYSQLX_OPT_HOST;
+    fmt::println("test = {}", std::to_underlying(test));
 }
