3f23c1d776f62b0c2ca083b1ceeb687598714275
sal <sal@orlac.io>
2025-03-04 17:09:53 -0500

    xieite testing

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,11 @@
 #include <fmt/color.h>
 #include <fmt/format.h>
 #include <fmt/std.h>
-#include <openssl/crypto.h>
+#include <xieite/io/dump.hpp>
 
 int main(int argc, char** argv)
 {
-    constexpr std::string_view hi{ "hello world" };
-    fmt::print(fmt::emphasis::blink | fmt::fg(fmt::color::coral), "{}\n", hi);
-
-    const uint32_t ssl_major{ OPENSSL_version_major() };
-    const uint32_t ssl_minor{ OPENSSL_version_minor() };
-    const uint32_t ssl_patch{ OPENSSL_version_patch() };
-
-    fmt::print(fmt::emphasis::underline | fmt::fg(fmt::color::beige),
-               "Using OpenSSL Version: {}.{}.{}\n", ssl_major, ssl_minor, ssl_patch);
-
-    std::cin.get();
+    xieite::dump("Hello, world!");
 
     return 0;
 }
